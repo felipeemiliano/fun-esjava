eed88c50d8149674f6717669e309df7a06fbc90d
felipeemiliano <111158344+felipeemiliano@users.noreply.github.com>
2022-09-03 11:35:37 -0300

    Create function.js

---
+++ function.js
@@ -0,0 +1,11 @@
+function bemvindo(){
+    console.log (" Bem vindo!!")
+}
+
+function soma(num2 , num2){
+    var resultado = num1+ num2;
+    console.log(resultado);
+}
+
+const calculadora=(operador,[num1 , num2]=[10, 5])=> "Resultado final" - eval ('${num1} ${operador} ${num2}');
+calculadora('-')
